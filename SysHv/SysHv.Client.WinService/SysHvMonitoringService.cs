eb821a929147a4d17b28deeb3285d2af660ef557
Andrew <andrew.1.kulikov@gmail.com>
2019-03-28 23:27:37 +0300

    WinClient-2 Create client windows service application

---
+++ SysHv/SysHv.Client.WinService/SysHvMonitoringService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace SysHv.Client.WinService
+{
+    public partial class SysHvMonitoringService : ServiceBase
+    {
+        public SysHvMonitoringService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}

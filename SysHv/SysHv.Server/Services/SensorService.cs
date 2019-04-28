a89c2e4e6b88c7fdce2cdef40268108b81bf64c9
Andrew <andrew.1.kulikov@gmail.com>
2019-04-28 11:45:18 +0300

    Server-44 Implement methods in sensor service

--- SysHv/SysHv.Server/Services/SensorService.cs
+++ SysHv/SysHv.Server/Services/SensorService.cs
@@ -2,30 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SysHv.Server.DAL;
 using SysHv.Server.DAL.Models;
 
 namespace SysHv.Server.Services
 {
-    public class SensorService : ISensorService
+    public class SensorService : ISensorService, IDisposable
     {
+        private readonly ServerDbContext _context;
+
+        public SensorService(ServerDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<Sensor> GetSensorByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return _context.Sensors.FirstOrDefaultAsync(s => s.Id == id);
         }
 
         public Task<Sensor> GetSensorByNameAsync(string name)
         {
-            throw new NotImplementedException();
+            return _context.Sensors.FirstOrDefaultAsync(s => s.Name == name);
         }
 
-        public Task<ICollection<Sensor>> GetAllSensorsAsync()
+        public Task<List<Sensor>> GetAllSensorsAsync()
         {
-            throw new NotImplementedException();
+            return _context.Sensors.ToListAsync();
         }
 
-        public Task<ICollection<Sensor>> GetClientSensorsAsync(int clientId)
+        public Task<List<Sensor>> GetClientSensorsAsync(int clientId)
         {
-            throw new NotImplementedException();
+            return _context.ClientSensors.Where(s => s.ClientId == clientId).Select(s => s.Sensor).ToListAsync();
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
         }
     }
 }

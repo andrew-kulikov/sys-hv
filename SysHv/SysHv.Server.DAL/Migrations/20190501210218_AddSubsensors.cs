a02518a03b7c9678f4bcaa7f4d4ff06c1515ec6e
Andrew <andrew.1.kulikov@gmail.com>
2019-05-02 00:06:50 +0300

    Server-42 Add subsensor

---
+++ SysHv/SysHv.Server.DAL/Migrations/20190501210218_AddSubsensors.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SysHv.Server.DAL.Migrations
+{
+    public partial class AddSubsensors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SubSensor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    MinValue = table.Column<float>(nullable: true),
+                    MaxValue = table.Column<float>(nullable: true),
+                    CriticalValue = table.Column<float>(nullable: true),
+                    IsNumeric = table.Column<bool>(nullable: false),
+                    SensorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubSensor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubSensor_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubSensor_SensorId",
+                table: "SubSensor",
+                column: "SensorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubSensor");
+        }
+    }
+}
--- SysHv/SysHv.Server.DAL/Models/SubSensor.cs
+++ SysHv/SysHv.Server.DAL/Models/SubSensor.cs
@@ -4,17 +4,16 @@ using System.Text;
 
 namespace SysHv.Server.DAL.Models
 {
-    public class Sensor
+    public class SubSensor
     {
-        public int Id { get; set; }    
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ReturnType { get; set; }
-        public bool IsNumeric { get; set; }
         public float? MinValue { get; set; }
         public float? MaxValue { get; set; }
         public float? CriticalValue { get; set; }
-        public string Contract { get; set; }
-        public ICollection<SubSensor> SubSensors { get; set; }
+        public bool IsNumeric { get; set; }
+        public int SensorId { get; set; }
+        public Sensor Sensor { get; set; }
     }
 }

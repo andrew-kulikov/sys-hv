923451d7b7cb081657c9f33fed7b2f13ae1348d6
Andrew <andrew.1.kulikov@gmail.com>
2019-05-02 00:40:26 +0300

    Server-42 Add CPULoadSensorDto model to send CPU collected data

---
+++ SysHv/SysHv.Client.Common/DTOs/SensorOutput/CPULoadSensorDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysHv.Client.Common.DTOs.SensorOutput
+{
+    public class CPULoadSensorDto
+    {
+        public class CPUCoreLoadDto
+        {
+            public string CoreName { get; set; }
+            public float Load { get; set; } 
+        }
+        public float TotalLoad { get; set; }
+        public IEnumerable<CPUCoreLoadDto> CoreLoads { get; set; }
+    }
+}

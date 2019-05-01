b715580216cc089624b371bef8bc5e7acca0f857
Andrew <andrew.1.kulikov@gmail.com>
2019-05-02 00:17:27 +0300

    Server-42 Rename SubSensor table to SubSensors

--- SysHv/SysHv.Server.DAL/Migrations/20190501211550_RenameSubSensors.Designer.cs
+++ SysHv/SysHv.Server.DAL/Migrations/20190501211550_RenameSubSensors.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SysHv.Server.DAL;
 
 namespace SysHv.Server.DAL.Migrations
 {
     [DbContext(typeof(ServerDbContext))]
-    partial class ServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190501211550_RenameSubSensors")]
+    partial class RenameSubSensors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SysHv/SysHv.Server.DAL/ServerDbContext.cs
+++ SysHv/SysHv.Server.DAL/ServerDbContext.cs
@@ -13,6 +13,7 @@ namespace SysHv.Server.DAL
         public DbSet<Sensor> Sensors { get; set; }
         public DbSet<Client> Clients { get; set; }
         public DbSet<ClientSensor> ClientSensors { get; set; }
+        public DbSet<SubSensor> SubSensors { get; set; }
         public ServerDbContext(DbContextOptions<ServerDbContext> options) : base(options)
         {
 

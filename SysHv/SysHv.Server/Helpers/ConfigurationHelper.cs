29194e0b49fd942b21e3cddddaa64d660a045c5b
Andrew <andrew.1.kulikov@gmail.com>
2019-05-01 21:04:21 +0300

    Server-52 Add ConfigurationHelper class

---
+++ SysHv/SysHv.Server/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SysHv.Client.Common.Models;
+
+namespace SysHv.Server.Helpers
+{
+    public class ConfigurationHelper : IConfigurationHelper
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public ConnectionModel ConnectionInfo => new ConnectionModel(
+            _configuration.GetValue<string>("Host"),
+            _configuration.GetValue<string>("Username"),
+            _configuration.GetValue<string>("Password")
+            );
+    }
+}
---
+++ SysHv/SysHv.Server/Helpers/IConfigurationHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SysHv.Client.Common.Models;
+
+namespace SysHv.Server.Helpers
+{
+    public interface IConfigurationHelper
+    {
+        ConnectionModel ConnectionInfo { get; }
+    }
+}

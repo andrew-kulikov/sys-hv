a40ad02dce1534a9862992b79f450dee2ff5d973
ololoev <tur4ikkk@mail.ru>
2019-05-06 15:23:17 +0300

    add demo sql setup for database

---
+++ Deployment/DemoSensorsSetup.sql
@@ -0,0 +1,47 @@
+﻿-- values setup script. Execute after Update-Database applied for SysHv.Server
+truncate table dbo.SubSensors;
+truncate table dbo.ClientSensors;
+
+-- drop constraints
+
+alter table dbo.SubSensors
+drop CONSTRAINT [FK_SubSensors_Sensors_SensorId];
+
+
+alter table dbo.ClientSensors
+drop CONSTRAINT [FK_ClientSensors_Clients_ClientId],
+     CONSTRAINT [FK_ClientSensors_Sensors_SensorId];
+
+-- truncate referenced table
+truncate table dbo.Sensors;
+
+-- restore constraints
+alter table dbo.SubSensors
+add CONSTRAINT [FK_SubSensors_Sensors_SensorId] FOREIGN KEY ([SensorId]) REFERENCES [dbo].[Sensors] ([Id]) ON DELETE CASCADE;
+
+alter table dbo.ClientSensors
+add CONSTRAINT [FK_ClientSensors_Clients_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Clients] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_ClientSensors_Sensors_SensorId] FOREIGN KEY ([SensorId]) REFERENCES [dbo].[Sensors] ([Id]) ON DELETE CASCADE;
+
+-- sensors
+insert into dbo.Sensors
+values ('CPU total temp', null, 90, 1, 100, 0, 'CPULoadSensorDto', 'CpuTempSensor', 0);
+
+insert into dbo.Sensors
+values ('CPU total load', null, 90, 1, 100, 0, 'CPULoadSensorDto', 'CpuLoadSensor', 0);
+
+
+-- sub sensors
+insert into dbo.SubSensors
+values ('Core temperature sensor', null, 0, 100, 90, 1, 1);
+
+insert into dbo.SubSensors
+values ('Core load sensor', null, 0, 100, 90, 1, 2);
+
+
+-- client sensors
+insert into dbo.ClientSensors
+values ('Test Sensor (Cpu temp)', null, 1, 1, 5000);
+
+insert into dbo.ClientSensors
+values ('Test Sensor (Cpu load)', null, 1, 2, 10000);
--- SysHv/SysHv.WinAdminClient/WinAdminClientCore/WinAdminClientCore/ViewModels/Sensors/SingleNumberSensorViewModel.cs
+++ SysHv/SysHv.WinAdminClient/WinAdminClientCore/WinAdminClientCore/ViewModels/Sensors/SingleNumberSensorViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace WinAdminClientCore.ViewModels.Sensors
+﻿using System.Collections;
+using System.Linq;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace WinAdminClientCore.ViewModels.Sensors
 {
     public class SingleNumberSensorViewModel : ViewModelBase
     {
-        public double CurrentValue { get; set; }
+        private SeriesCollection _observableDoubleSeries;
+
+        private LineSeries _observableLine;
+
+        public double LastValue {
+            get
+            {
+                var a =(double) (_observableLine.Values as ICollection).;
+            }
+            set; }
     }
 }
